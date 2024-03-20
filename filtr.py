54ec09760fa61e35ae4d4a9338140e23936fb618
Jakub Fiala <fia42192@spseol.cz>
2024-03-20 13:29:37 +0100

    přidání funkce

--- filtr.py
+++ filtr.py
@@ -1,18 +1,20 @@
 from PIL import Image
 obrazek = Image.open("poecilotheria.jpg")
 sirka, vyska = obrazek.size
-x = 0
-while x < sirka:
-    y = 0
-    while y < vyska:
-        r, g, b = obrazek.getpixel((x,y))
-        prumer = int((r+g+b)/2.4)
-        obrazek.putpixel((x,y), (r , b, r))
-        if prumer > 150:
-            obrazek.putpixel((x,y), (255, 255, 255))
-        else:
-            obrazek.putpixel((x,y), (100, 130, 90))
-        y += 1
-    x += 1
+def uprava_barev():
+    x = 0    
+    while x < sirka:
+        y = 0
+        while y < vyska:
+            r, g, b = obrazek.getpixel((x,y))
+            prumer = int((r+g+b)/2.4)
+            obrazek.putpixel((x,y), (r , b, r))
+            if prumer > 150:
+                obrazek.putpixel((x,y), (255, 255, 255))
+            else:
+                obrazek.putpixel((x,y), (100, 130, 90))
+            y += 1
+        x += 1
+    obrazek.show()
 # display(obrazek) #obrazek.show()
-obrazek.show()
+uprava_barev()
