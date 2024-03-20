11e5ed273fb34daf0f53a57f3ab22963b3a40b0b
Jakub Fiala <fia42192@spseol.cz>
2024-03-20 13:16:23 +0100

    funkční základní verze filtru

--- filtr.py
+++ filtr.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+from PIL import Image
+obrazek = Image.open("poecilotheria.jpg")
+sirka, vyska = obrazek.size
+x = 0
+while x < sirka:
+    y = 0
+    while y < vyska:
+        r, g, b = obrazek.getpixel((x,y))
+        prumer = int((r+g+b)/2.4)
+        obrazek.putpixel((x,y), (r , b, r))
+        if prumer > 150:
+            obrazek.putpixel((x,y), (255, 255, 255))
+        else:
+            obrazek.putpixel((x,y), (100, 130, 90))
+        y += 1
+    x += 1
+# display(obrazek) #obrazek.show()
+obrazek.show()
